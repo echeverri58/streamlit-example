eafd24b4787a10ff301521c27102e2f45ed453c1
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2023-11-23 07:45:07 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,29 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-"""
-# Welcome to Streamlit!
+def generate_random_data():
+    # Función para generar datos aleatorios
+    x = np.linspace(0, 10, 100)
+    y = np.random.randn(100)
+    return x, y
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def plot_random_graph():
+    # Función para crear y mostrar una gráfica aleatoria
+    x, y = generate_random_data()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, label='Datos aleatorios')
+    plt.title('Gráfica Aleatoria')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    
+    st.pyplot(plt)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+def main():
+    st.title('Visualización de Datos Aleatorios')
+    st.sidebar.button('Generar nueva gráfica', on_click=plot_random_graph)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if __name__ == "__main__":
+    main()
