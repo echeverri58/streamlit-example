66b4e152da52e31c7bfc4d993a8c54bc60995d34
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2023-11-23 19:29:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,38 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import folium
+from streamlit_folium import folium_static
 
-def generate_random_data():
-    # Función para generar datos aleatorios
-    x = np.linspace(0, 10, 100)
-    y = np.random.randn(100)
-    return x, y
+# Generar datos aleatorios para las dos columnas
+data_columna1 = pd.DataFrame({
+    'Categoría': ['A', 'B', 'C', 'D'],
+    'Valor': np.random.randint(1, 10, size=4)
+})
 
-def plot_random_graph():
-    # Función para crear y mostrar una gráfica aleatoria
-    x, y = generate_random_data()
+data_columna2 = pd.DataFrame({
+    'Categoría': ['E', 'F', 'G', 'H'],
+    'Valor': np.random.randint(1, 10, size=4)
+})
 
-    plt.figure(figsize=(8, 6))
-    plt.plot(x, y, label='Datos aleatorios')
-    plt.title('Gráfica Aleatoria')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend()
-    
-    st.pyplot(plt)
+# Crear el mapa de Medellín
+medellin_map = folium.Map(location=[6.2442, -75.5812], zoom_start=12)
 
-def main():
-    st.title('Visualización de Datos Aleatorios')
-    st.sidebar.button('Generar nueva gráfica', on_click=plot_random_graph)
+# Generar 50 ubicaciones geográficas aleatorias en Medellín
+random_locations = [(6.2442 + np.random.uniform(-0.1, 0.1), -75.5812 + np.random.uniform(-0.1, 0.1)) for _ in range(50)]
 
-if __name__ == "__main__":
-    main()
+# Señalizar los puntos en el mapa como círculos
+for location in random_locations:
+    folium.Circle(location, radius=100, color='blue', fill=True, fill_color='blue').add_to(medellin_map)
+
+# Crear dos columnas para los gráficos
+col1, col2 = st.columns(2)
+
+# Gráficos de barras en las columnas
+col1.bar_chart(data_columna1.set_index('Categoría'))
+col2.bar_chart(data_columna2.set_index('Categoría'))
+
+# Mostrar el mapa debajo de las columnas
+st.markdown("### Mapa de Medellín con Puntos Aleatorios")
+folium_static(medellin_map)
+
